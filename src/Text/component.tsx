640b7ab74a5ab8bef013c133f76fa65953a08c30
Mikita Shamkuts <mikitashamkuts@gmail.com>
2024-02-10 13:34:59 +0000

    feat: text component demo publishing

--- src/Text/component.tsx
+++ src/Text/component.tsx
@@ -1,8 +1,8 @@
 import React, { type FC, memo } from 'react';
 import { Text as RNText } from 'react-native';
 
-const Text: FC = ({}) => {
-  return <RNText>Text</RNText>;
+const Text: FC = ({ children }) => {
+  return <RNText>{children}</RNText>;
 };
 
 export default memo(Text);
