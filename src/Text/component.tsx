f2ae68f3c2d5b283898a126d064637f77b15b69e
Mikita Shamkuts <mikitashamkuts@gmail.com>
2024-02-10 13:06:27 +0000

    feat: text component demo setup

---
+++ src/Text/component.tsx
@@ -0,0 +1,8 @@
+import React, { type FC, memo } from 'react';
+import { Text as RNText } from 'react-native';
+
+const Text: FC = ({}) => {
+  return <RNText>Text</RNText>;
+};
+
+export default memo(Text);
