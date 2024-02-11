f177561e52c9d6a3c8514643070052d562527018
Mikita Shamkuts <mikitashamkuts@gmail.com>
2024-02-11 15:36:32 +0000

    feat: release config check

--- src/Text/component.tsx
+++ src/Text/component.tsx
@@ -1,8 +1,17 @@
 import React, { type FC, memo } from 'react';
 import { Text as RNText } from 'react-native';
 
-const Text: FC = ({ children }) => {
-  return <RNText className="text-red">{children}</RNText>;
+type Props = {
+  test: any;
+};
+
+const Text: FC<Props> = ({ children, test }) => {
+  return (
+    <RNText className="text-red">
+      {children}
+      {test}
+    </RNText>
+  );
 };
 
 export default memo(Text);
